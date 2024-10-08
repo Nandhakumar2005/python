65879218df5d64d25eb2b2ac76db713e4daaf5bb
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-10-08 16:24:33 +0530

    Add files via upload

---
+++ task 3.py
@@ -0,0 +1,20 @@
+'''
+AUTHOR:Nandhakumar KS
+DATE:8-10-2024
+Program to demonstrate the usage of arithmetic and logical operators
+'''
+
+number1=10
+number2=5
+addition=number1+number2
+division=number1/number2
+greater=number1>number2
+equal=number1==number2
+_and=greater and not equal
+_or=greater or equal
+
+print("sum:",addition,',','division:',division)
+
+print("is a greater than b?:",greater)
+
+print('are a and b equal:',equal)
