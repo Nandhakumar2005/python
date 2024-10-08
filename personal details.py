7021d584d2e8ebe0572620fcd7cffc6dbc8906d2
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-10-08 15:48:28 +0530

    Add files via upload

---
+++ personal details.py
@@ -0,0 +1,10 @@
+'''
+AUTHOR:Nandhakumar KS
+DATE:8-10-2024
+Python program that asks the user their name and age
+and display the output including their name and age
+'''
+
+name=input('enter your name: ')
+age=int(input('enter your age: '))
+print("hello,",name,'!',"you are",age,"years old")
