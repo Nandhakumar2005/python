0313c867763e863b890dd27afea243d4f415ba49
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-10-08 15:49:09 +0530

    Add files via upload

---
+++ string_catenation.py
@@ -0,0 +1,10 @@
+'''
+AUTHOR:Nandhakumar KS
+DATE:8-10-2024
+Python program to extract a specific part of the string(substring)
+and then concatenate it with another string.Finally,display the new string
+'''
+
+str1="hello world!"
+str2=str1[6:11]
+str3="Everyone!"+' '+str2
