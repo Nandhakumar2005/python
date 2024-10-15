e642aca65e13a1e5561a6a33d98275c526319c79
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-10-15 15:20:20 +0530

    Add files via upload

---
+++ twinkle twinkle.py
@@ -0,0 +1,7 @@
+'''
+AUTHOR:Nandhakumar KS
+DATE:10-10-2024
+To write a python program to print a string/sentence.
+'''
+
+print("Twinkle,twinkle little star,\n\tHow I wonder what you are,\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky,\nTwinkle,twinkle little star\n\tHow I wonder what you are")
