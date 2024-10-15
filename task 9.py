a3262f1042ddcb38e6543971e5da401b82696a37
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-10-15 14:03:03 +0530

    Add files via upload

---
+++ task 9.py
@@ -0,0 +1,11 @@
+'''
+AUTHOR:Nandhakumar KS
+DATE:15-10-2024
+To check whether the given number is positive or not.
+'''
+
+number=int(input("Enter a number: "))
+if number>0:
+    print("The given number is: ",number,"is positive.")
+else:
+    print("The given number is: ",number,"is negative.")
