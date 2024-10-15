1fe14ce0527e64b0b160afc0626d685c01a58428
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-10-15 14:11:47 +0530

    Add files via upload

--- task 9.py
+++ task 9.py
@@ -7,5 +7,7 @@ To check whether the given number is positive or not.
 number=int(input("Enter a number: "))
 if number>0:
     print("The given number is: ",number,"is positive.")
-else:
+elif number<0:
     print("The given number is: ",number,"is negative.")
+else:
+    print("the given number:",number,"is zero")
